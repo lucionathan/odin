7de476e8ce623d2a24d965e8f44ecd23e2ff029a
lucionathan <lucio.matias@ccc.ufcg.edu.br>
2021-09-04 13:10:23 -0300

    grid and clear grid done

--- fundamentals/etch-a-sketch/index.js
+++ fundamentals/etch-a-sketch/index.js
@@ -1,13 +1,34 @@
-const container = document.getElementById('container');
+const color = '#333333';
 
-function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-columns', cols);
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement('div');
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
-  };
+const grid = document.getElementById('grid');
+const clearButton = document.getElementById('clearBtn');
+
+clearButton.addEventListener('click', clearGrid)
+
+function createGrid(size) {
+	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+  
+	for (let i = 0; i < size * size; i++) {
+		const gridElement = document.createElement('div');
+		gridElement.addEventListener('mouseover', function() {
+			gridElement.style.backgroundColor = color;
+		})
+		grid.appendChild(gridElement).className = 'content';
+	}
+}
+
+function clearGrid() {
+	const content = document.getElementsByClassName('content'); 
+	for(let i = 0; i < content.length; i++) {
+		content[i].style.backgroundColor = 'white';
+	}
+	console.log('mizera');
+}
+
+
+
+window.onload = () => {
+	createGrid(16);
 };
-
-makeRows(16, 16);
+  
