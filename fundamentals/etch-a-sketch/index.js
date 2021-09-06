03de640678e3da686218151082bcdbead81afc16
Lucio Nathan <lucio.matias@ccc.ufcg.edu.br>
2021-09-06 20:41:59 -0300

    grid size added

--- fundamentals/etch-a-sketch/index.js
+++ fundamentals/etch-a-sketch/index.js
@@ -1,60 +1,66 @@
-const default_color = '#333333';
+const default_color = "#333333";
 const default_size = 16;
 
 let color = default_color;
 let size = default_size;
 
-const grid = document.getElementById('grid');
-const clearButton = document.getElementById('clearBtn');
-const colorButton = document.getElementById('colorBtn');
-const eraserButton = document.getElementById('eraserBtn');
-const sizeSlider = document.getElementById('sizeSlider');
+const grid = document.getElementById("grid");
+const clearButton = document.getElementById("clearBtn");
+const colorButton = document.getElementById("colorBtn");
+const eraserButton = document.getElementById("eraserBtn");
+const sizeSlider = document.getElementById("sizeSlider");
+const sizeValue = document.getElementById("sizeValue");
 
-eraserButton.addEventListener('click', eraserPen);
-colorButton.addEventListener('click', colorPen);
-clearButton.addEventListener('click', clearGrid);
-sizeSlider.addEventListener('change', (e) => changeSize(e.target.value));
+eraserButton.addEventListener("click", eraserPen);
+colorButton.addEventListener("click", colorPen);
+clearButton.addEventListener("click", clearGrid);
+sizeSlider.addEventListener("mousemove", (e) =>
+    updateSizeValue(e.target.value)
+);
+sizeSlider.addEventListener("change", (e) => changeSize(e.target.value));
+
+function updateSizeValue(value) {
+    sizeValue.innerText = `${value} x ${value}`;
+}
 
 function changeSize(newSize) {
-	size = newSize;
-	clearGrid();
-	createGrid(size);
+    size = newSize;
+    reloadGrid();
 }
 
 function reloadGrid() {
-
+    clearGrid();
+    createGrid(size);
 }
 
 function createGrid(size) {
-	grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-	grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
-  
-	for (let i = 0; i < size * size; i++) {
-		const gridElement = document.createElement('div');
-		gridElement.addEventListener('mouseover', function() {
-			gridElement.style.backgroundColor = color;
-		});
-		grid.appendChild(gridElement).className = 'content';
-	}
+    grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+    grid.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+
+    for (let i = 0; i < size * size; i++) {
+        const gridElement = document.createElement("div");
+        gridElement.addEventListener("mouseover", function () {
+            gridElement.style.backgroundColor = color;
+        });
+        grid.appendChild(gridElement).className = "content";
+    }
 }
 
 function clearGrid() {
-	const content = document.getElementsByClassName('content'); 
-	for(let i = 0; i < content.length; i++) {
-		content[i].style.backgroundColor = 'white';
-	}
+    const content = document.getElementsByClassName("content");
+    for (let i = 0; i < content.length; i++) {
+        content[i].style.backgroundColor = "white";
+    }
 }
 
 function eraserPen() {
-	color = 'white';
+    color = "white";
 }
 
 function colorPen() {
-	color = default_color;
+    color = default_color;
 }
 
-
 window.onload = () => {
-	createGrid(default_size);
+    createGrid(default_size);
 };
-  
